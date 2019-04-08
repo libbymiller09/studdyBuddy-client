e0bdee58df2797e24ee655fbf927d49029c4b306
Elisabeth Miller <libbymiiler09@gmail.com>
2019-04-08 14:14:44 -0700

    added api-base-url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,28 +1,31 @@
 import axios from 'axios';
 import { FETCH_USER, DELETE_MEETUP } from './types';
 import { FETCH_MEETUPS } from './types';
+const { API_BASE_URL } = require('./config/config');
+
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user')
+  const res = await axios.get(`${API_BASE_URL}/current_user`)
+  // const res = await axios.get('/api/current_user')
   
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const submitForm = (values, history) => async dispatch => {
-  const res = await axios.post('/api/meetups', values);
+  const res = await axios.post(`${API_BASE_URL}/meetups`, values);
 
   history.push('/meetups');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const fetchMeetups = () => async dispatch => {
-  const res = await axios.get('/api/meetups');
+  const res = await axios.get(`${API_BASE_URL}/meetups`);
 
   dispatch({ type: FETCH_MEETUPS, payload: res.data });
 };
 
 export const deleteMeetup = () => async dispatch => {
-  const res = await axios.delete('/api/meetups/:id');
+  const res = await axios.delete(`${API_BASE_URL}/meetups/:id`);
 
   dispatch({ type: DELETE_MEETUP, payload: res.data });
 }
