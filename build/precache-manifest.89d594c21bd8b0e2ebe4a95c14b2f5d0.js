9ddcdb66eeb813f9e16d8a35bffaf60811f8bfc5
Elisabeth Miller <libbymiiler09@gmail.com>
2019-04-08 13:47:10 -0700

    build file

---
+++ build/precache-manifest.89d594c21bd8b0e2ebe4a95c14b2f5d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f718e3c29839188e93dd",
+    "url": "/static/css/main.d4cb229d.chunk.css"
+  },
+  {
+    "revision": "f718e3c29839188e93dd",
+    "url": "/static/js/main.f718e3c2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2fefc2ec42c7346b582b",
+    "url": "/static/js/2.2fefc2ec.chunk.js"
+  },
+  {
+    "revision": "e65222a18f4744db1abb8bd8fea80a89",
+    "url": "/index.html"
+  }
+];
