2f4caa819be4c2433ae71bd8399b4a3bb5a26150
Elisabeth Miller <libbymiiler09@gmail.com>
2019-05-10 11:10:15 -0700

    minor refactor of action tests

--- src/actions/types.test.js
+++ src/actions/types.test.js
@@ -3,96 +3,87 @@ import {shallow, mount} from 'enzyme';
 import moxios from 'moxios';
 
 import { storeFactory } from '../test/testUtils';
-import { fetchMeetups, fetchUser, deleteMeetup } from './index';
+import { fetchMeetups, fetchUser, deleteMeetup, submitForm } from './index';
 
-describe('fetch_meetup action dispatcher', () => {
+describe('action dispatchers', () => {
   beforeEach(() => {
     moxios.install();
   });
   afterEach(() => {
     moxios.uninstall();
   });
-  it('add meetup to state', () => {
-    const meetup = {
-      subject: 'node.js',
-      time: 'noon',
-      place: 'Prince coffee',
-      _user: 'TestUser',
-    };
-    const store = storeFactory();
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent();
-      request.respondWith({
-        status: 200,
-        response: meetup,
+  describe('fetch_meetup action dispatcher', () => {
+    it('add meetup to state', () => {
+      const meetup = {
+        subject: 'node.js',
+        time: 'noon',
+        place: 'Prince coffee',
+        _user: 'TestUser',
+      };
+      const store = storeFactory();
+  
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: meetup,
+        });
       });
+  
+      return store.dispatch(fetchMeetups())
+        .then(() => {
+          const res = store.getState();
+        });
+        expect(res.meetup).toBe(meetup);
     });
-
-    return store.dispatch(fetchMeetups())
-      .then(() => {
-        const res = store.getState();
+  });
+  
+  describe('fetch_user action dispatcher', () => {
+    it('add meetup to state', () => {
+      const user = {
+        googleId: '1234',
+      };
+      const store = storeFactory();
+  
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: user,
+        });
       });
-      expect(res.meetup).toBe(meetup);
+  
+      return store.dispatch(fetchUser())
+        .then(() => {
+          const res = store.getState();
+        });
+        expect(res.user).toBe(meetup);
+    });
+  });
+  
+  describe('delete_meetup action dispatcher', () => {
+    it('delete meetup from state', () => {
+      const meetup = {
+        subject: 'node.js',
+        time: 'noon',
+        place: 'Prince coffee',
+      };
+      const store = storeFactory();
+  
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: meetup,
+        });
+      });
+  
+      return store.dispatch(deleteMeetup())
+        .then(() => {
+          const res = store.getState();
+        });
+        expect(res.meetup).toBe({});
+    });
   });
 });
 
-describe('fetch_user action dispatcher', () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-  afterEach(() => {
-    moxios.uninstall();
-  });
-  it('add meetup to state', () => {
-    const user = {
-      googleId: '1234',
-    };
-    const store = storeFactory();
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent();
-      request.respondWith({
-        status: 200,
-        response: user,
-      });
-    });
-
-    return store.dispatch(fetchUser())
-      .then(() => {
-        const res = store.getState();
-      });
-      expect(res.user).toBe(meetup);
-  });
-});
-
-describe('delete_meetup action dispatcher', () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-  afterEach(() => {
-    moxios.uninstall();
-  });
-  it('delete meetup from state', () => {
-    const meetup = {
-      subject: 'node.js',
-      time: 'noon',
-      place: 'Prince coffee',
-    };
-    const store = storeFactory();
-
-    moxios.wait(() => {
-      const request = moxios.requests.mostRecent();
-      request.respondWith({
-        status: 200,
-        response: meetup,
-      });
-    });
-
-    return store.dispatch(deleteMeetup())
-      .then(() => {
-        const res = store.getState();
-      });
-      expect(res.meetup).toBe({});
-  });
-});
