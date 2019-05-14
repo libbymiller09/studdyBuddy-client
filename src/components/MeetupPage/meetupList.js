bf3ca3dc6dd31a0521066824156385d55972f5cb
Elisabeth Miller <libbymiiler09@gmail.com>
2019-05-14 12:33:01 -0700

    revert renderMeetups

--- src/components/MeetupPage/meetupList.js
+++ src/components/MeetupPage/meetupList.js
@@ -3,13 +3,6 @@ import { connect } from 'react-redux';
 import { fetchMeetups } from '../../actions';
 
 class MeetupList extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.props = {
-  //     meetups: null,
-  //   };
-  // }
 
   componentDidMount() {
     this.props.fetchMeetups();
@@ -22,43 +15,20 @@ class MeetupList extends Component {
   };
 
   renderMeetups() {
-    return (
-      <div>
-        <div className="meetupContent">
-          {(this.props.meetups || []).map(meetup => (
-            <div key={meetup._id}>
-              <h3 className="subject">Subject: {meetup.subject}</h3>
-              <h3 className="time">Time: {meetup.time}</h3>
-              <h3 className="place">Place: {meetup.place}</h3>
-              <button onClick={() => { completedButton(meetup._id) }} className="check-toggle" type="button">Completed</button>
-            </div>
-          ))}
+    return this.props && this.props.meetups && this.props.meetups.map(function(meetup) {
+      // return Object.values(this.props.meetups).forEach(meetup => {
+      return (
+        <div key={meetup._id} >
+          <div className="meetupContent">
+            <h3 className="subject">Subject: {meetup.subject}</h3>
+            <h3 className="time">Time: {meetup.time}</h3>
+            <h3 className="place">Place: {meetup.place}</h3>
+          </div>
+          <button onClick={() => { completedButton(meetup._id) }} className="check-toggle" type="button">Completed</button>
         </div>
-      </div>
-    );
-
-    // let meetups = 
-    //   this.props.meetups !== undefined
-    //     ? this.props.meetups 
-    //     : (this.props.trigger === 'hover' ? 100: 0)
-
-    //   return this.props && this.props.meetups && this.props.meetups.map(function(meetup) {
-    //     // return Object.values(this.props.meetups).forEach(meetup => {
-          
-    //     return (
-    //       <div key={meetup._id} >
-    //         <div className="meetupContent">
-    //           <h3 className="subject">Subject: {meetup.subject}</h3>
-    //           <h3 className="time">Time: {meetup.time}</h3>
-    //           <h3 className="place">Place: {meetup.place}</h3>
-    //         </div>
-    //         <button onClick={() => { completedButton(meetup._id) }} className="check-toggle" type="button">Completed</button>
-    //       </div>
-    //     );
-    //   });
-    };
-  
-    
+      );
+    });
+  };
 
   render() {
     return (
