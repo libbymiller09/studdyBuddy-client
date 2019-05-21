57d53d3c7eefa48df2ce32cbe74dedcc8c39c956
Elisabeth Miller <libbymiiler09@gmail.com>
2019-05-21 15:29:07 -0700

    confgi

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 5000,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
- 'https://desolate-lowlands-90762.herokuapp.com'
+  // 'http://localhost:3000/api' 
+  'https://warm-atoll-66734.herokuapp.com/'
 };
-// 'http://localhost:3000/api'
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use('/auth/google', proxy({ target: 'https://desolate-lowlands-90762.herokuapp.com' }));
-  app.use('/api/*', proxy({ target: 'https://desolate-lowlands-90762.herokuapp.com' }));
+  // app.use('/auth/google', proxy({ target: 'https://warm-atoll-66734.herokuapp.com/' }));
+  // app.use('/api/*', proxy({ target: 'https://warm-atoll-66734.herokuapp.com/' }));
 
-//   app.use('/auth/google', proxy({ target: 'http://localhost:5000' }));
-//   app.use('/api/*', proxy({ target: 'http://localhost:5000' }));
+  app.use('/auth/google', proxy({ target: 'http://localhost:5000' }));
+  app.use('/api/*', proxy({ target: 'http://localhost:5000' }));
 }
